5692087d33644ba7e2fe3f3436fe589a007a007e
Reuven Podmazo <reuven@enigma.co>
2020-11-25 15:34:10 +0200

    added serialization helpers

--- src/contract.rs
+++ src/contract.rs
@@ -1,7 +1,8 @@
-use bincode2;
+use std::fmt::Debug;
+
 use cosmwasm_std::{
     Api, Binary, Env, Extern, HandleResponse, HandleResult, InitResponse, InitResult, Querier,
-    QueryResult, Storage,
+    QueryResult, StdError, StdResult, Storage,
 };
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -17,12 +18,10 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
     _env: Env,
     msg: InitMsg,
 ) -> InitResult {
-    deps.storage
-        .set(b"poll", &bincode2::serialize(&msg.poll).unwrap());
+    deps.storage.set(b"poll", &serialize(&msg.poll)?);
 
     let new_tally = Tally { yes: 0, no: 0 };
-    deps.storage
-        .set(b"tally", &bincode2::serialize(&new_tally).unwrap());
+    deps.storage.set(b"tally", &serialize(&new_tally)?);
     Ok(InitResponse::default())
 }
 
@@ -31,7 +30,7 @@ pub struct HandleMsg {
     yes: bool,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Tally {
     yes: u64,
     no: u64,
@@ -42,7 +41,7 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     _env: Env,
     msg: HandleMsg,
 ) -> HandleResult {
-    let mut tally: Tally = bincode2::deserialize(&deps.storage.get(b"tally").unwrap()).unwrap();
+    let mut tally: Tally = deserialize(&deps.storage.get(b"tally").unwrap())?;
 
     if msg.yes {
         tally.yes += 1;
@@ -50,8 +49,7 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
         tally.no += 1;
     }
 
-    deps.storage
-        .set(b"tally", &bincode2::serialize(&tally).unwrap());
+    deps.storage.set(b"tally", &serialize(&tally)?);
     Ok(HandleResponse::default())
 }
 
@@ -65,12 +63,22 @@ pub enum QueryMsg {
 pub fn query<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>, msg: QueryMsg) -> QueryResult {
     match msg {
         QueryMsg::GetPoll {} => {
-            let poll: String = bincode2::deserialize(&deps.storage.get(b"poll").unwrap()).unwrap();
+            let poll: String = deserialize(&deps.storage.get(b"poll").unwrap())?;
             Ok(Binary(serde_json_wasm::to_vec(&poll).unwrap()))
         }
         QueryMsg::GetTally {} => {
-            let tally: Tally = bincode2::deserialize(&deps.storage.get(b"tally").unwrap()).unwrap();
+            let tally: Tally = deserialize(&deps.storage.get(b"tally").unwrap())?;
             Ok(Binary(serde_json_wasm::to_vec(&tally).unwrap()))
         }
     }
+}
+
+fn serialize<T: Serialize + Debug>(value: &T) -> StdResult<Vec<u8>> {
+    bincode2::serialize(value)
+        .map_err(|_err| StdError::generic_err(format!("Failed to serialize object: {:?}", value)))
+}
+
+fn deserialize<'a, T: Deserialize<'a> + Debug>(data: &'a [u8]) -> StdResult<T> {
+    bincode2::deserialize(data)
+        .map_err(|_err| StdError::generic_err(format!("Failed to serialize object: {:?}", data)))
 }
